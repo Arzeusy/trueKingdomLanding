2f9c48936509c36c2d0d1af562dc70f4a1ec7690
Arzeusy <jkob1994@gmai.com>
2023-07-21 18:28:48 -0600

    se agrego imagen y la propuesta univa de valor, para la aplicacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
 /* eslint-disable react/jsx-no-undef */
 import Image from 'next/image'
 import React from 'react'
+import appPic from "../../public/images/Wood-Hand.svg"
+import AnimatedText from '@/components/AnimatedText'
+
 
 export default function Home() {
   return (
-    <main className=" flex items-center text-dark w-full min-h-screen">
-      <div>
-        home
+    <main className=" flex  text-dark w-full min-h-screen">
+      <div className='pt-0'>
+        <div className='flex items-center justify- w-full'>
+          <div className='w-3/5 flex flex-col items-center self-center'>
+              <AnimatedText 
+                text="Discover innovative learning with cultural immersion and flexible access" 
+                className='!text-5xl !text-right'  
+              />
+              <br />
+              <br />
+              <p className='my-4 px-4 text-base font-medium text-justify '>
+                Alexandria is an open-source mobile application that will assist in managing learning, personalization, and sharing your drafts.
+              </p>
+          </div>
+          <div className='w-2/5'>
+            <Image src={appPic} alt='codeBucks' className='w-full' />
+          </div>
+        </div>
       </div>
     </main>
   )
