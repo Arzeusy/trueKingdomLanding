3e41c8912cabb384882bebdcc25230fbeb756876
Arzeusy <jkob1994@gmai.com>
2023-08-18 21:57:32 -0600

    fix (Projects page) se corrigio las apostrofes encontrandas

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -20,12 +20,12 @@ const Projects = () => {
                 <div className="grid w-full grid-cols-8 gap-16 pt-16 ">
                     <div className="col-span-3 flex flex-col items-start justify-start">
                         <h2 data-testid="SubTitle" className="mb-4 text-lg font-bold uppercase text-dark/75">Biography</h2>
-                        <h4 className="text-lg font-medium"> Hi, I’m Yeicob. Nice to meet you. </h4>
+                        <h4 className="text-lg font-medium"> Hi, I&apos;m Yeicob. Nice to meet you. </h4>
                         <p className="font-medium my-4 text-justify">
-                        Since taking my first steps as a developer in both back-end and front-end roles about 4 years ago, I've had the opportunity to work with some of the most cutting-edge technologies in the world of development. My journey encompasses experiences in remote development teams and collaboration to craft comprehensive digital solutions. 
+                        Since taking my first steps as a developer in both back-end and front-end roles about 4 years ago, I&apos;ve had the opportunity to work with some of the most cutting-edge technologies in the world of development. My journey encompasses experiences in remote development teams and collaboration to craft comprehensive digital solutions. 
                         </p>
                         <p className="font-medium my-4 text-justify">
-                        possess a calm confidence in my abilities, an innate curiosity that propels me to keep learning, and an unwavering dedication to honing my skills by solving developmental challenges. Moreover, I'm currently delving into the realm of DevOps culture, allowing me to grasp the critical intersection between development and operations. My experience spans from collaborations with development companies to being a part of the internal teams of renowned brands, providing me with a diverse perspective in the creation and delivery of technological solutions.
+                        possess a calm confidence in my abilities, an innate curiosity that propels me to keep learning, and an unwavering dedication to honing my skills by solving developmental challenges. Moreover, I&apos;m currently delving into the realm of DevOps culture, allowing me to grasp the critical intersection between development and operations. My experience spans from collaborations with development companies to being a part of the internal teams of renowned brands, providing me with a diverse perspective in the creation and delivery of technological solutions.
                         </p>
                     </div>
                     <div className=" col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-gray-100 p-8">
